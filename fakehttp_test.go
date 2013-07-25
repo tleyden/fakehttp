39777c1240a25cf4037577a25dab5f503e7162c8
Traun Leyden <traun@couchbase.com>
2013-07-24 19:20:20 -0700

    fakehttp - borked from https://github.com/soundcloud/goamz/tree/master/testutil

---
+++ fakehttp_test.go
@@ -0,0 +1,36 @@
+package fakehttp
+
+import (
+	"fmt"
+	"github.com/couchbaselabs/go.assert"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+func TestUsage(t *testing.T) {
+
+	// startup fake server
+	var testServer = NewHTTPServer()
+	testServer.Start()
+
+	// tell it to respond with 200 status and this fake response
+	// on the next request it receives
+	fakeResponse := "<html>foo</html>"
+	testServer.Response(200, nil, fakeResponse)
+
+	// send a request to the fake server and read response
+	urlString := fmt.Sprintf("%s/foo.html", testServer.URL)
+	res, err := http.Get(urlString)
+	if err != nil {
+		panic("unexpected error")
+	}
+	responseBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic("unexpected error")
+	}
+
+	// make sure that the response is our fake response
+	assert.True(t, string(responseBody) == fakeResponse)
+
+}
